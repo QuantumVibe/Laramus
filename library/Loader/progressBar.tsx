8f6db7ee22d4d82b14020c9a578cd5075cafd41f
qiraxyz <ariqaziz74@gmail.com>
2023-12-22 22:38:09 +0700

    upload template

---
+++ library/Loader/progressBar.tsx
@@ -0,0 +1,17 @@
+// useLoading.tsx
+import { useEffect, useState } from "react";
+
+export default function progressBar() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return isLoading;
+}
